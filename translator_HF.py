50726243ef807f0d2d29f08f8ffc636ec9544188
Ludovico Chieffallo <ludovico_chieffallo@MacBook-Pro-di-Ludovico.local>
2026-02-03 18:13:30 +0100

    updated some script x

--- translator_HF.py
+++ translator_HF.py
@@ -1,4 +1,5 @@
+#it works only if you have transformers 4.44.2 library installed
 from transformers import pipeline
 
-translator = pipeline("translation", model="Helsinki-NLP/opus-mt-en-it")
+translator = pipeline("translation_en_to_it", model="Helsinki-NLP/opus-mt-en-it")
 print(translator("Hello, how are you?")[0]["translation_text"])
