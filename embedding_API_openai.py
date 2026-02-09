f163b5c897ba22fb0a0a681cdea246da2bd25d2d
Ludovico-Chieffallo <ludovico.chieffallo@outlook.it>
2026-02-09 15:34:42 +0100

    updated embeddings

--- embedding_API_openai.py
+++ embedding_API_openai.py
@@ -19,4 +19,31 @@ else:
     except Exception as e:
         print("\nSi è verificato un errore durante la chiamata all'API di OpenAI:")
         print(repr(e))  
+sentences = [
+    "The cat is on the table.",
+    "The dog is in the garden.",
+    "tomorrow is a sunny day.",
+]
+openai_embeddings_np = None
 
+if client:
+    try:
+        openai_model_name = "text-embedding-ada-002"
+        print(f"Richiedendo embedding con il modello {openai_model_name}...")
+        response = client.embeddings.create(
+            model=openai_model_name,
+            input= sentences
+        )
+        openai_embeddings_list = [item.embedding for item in response.data]
+        openai_embeddings_np = np.array(openai_embeddings_list)
+        print("Embeddings generati con successo.")
+        print(f"Embeddings shape: {openai_embeddings_np.shape}")
+        print(f"First embedding vector: {openai_embeddings_np[0][:5]}...")
+    except Exception as e:
+        print("\nSi è verificato un errore durante la generazione degli embedding con OpenAI:")
+        print(repr(e))
+    except openai.APIError as e:
+        print("\nSi è verificato un errore API durante la generazione degli embedding con OpenAI:")
+        print(repr(e))
+else:
+    print("Client OpenAI non disponibile, non è stato possibile generare gli embedding.")
