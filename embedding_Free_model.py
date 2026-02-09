246d8a55b56b543e5857cae1446932e8f8790674
Ludovico-Chieffallo <ludovico.chieffallo@outlook.it>
2026-02-09 16:27:18 +0100

    similarity updated

--- embedding_Free_model.py
+++ embedding_Free_model.py
@@ -13,8 +13,8 @@ print("libreries imported successfully")
 
 
 sentences = [
-    "The cat is on the table.",
-    "The dog is in the garden.",
+    "The cat is on the table in the garden.",
+    "The cat is in the garden.",
     "tomorrow is a sunny day.",
 ]
 
@@ -36,3 +36,19 @@ st_embeddings = model.encode(sentences)
 print("embeddings generated successfully")
 print(f"Embeddings shape: {st_embeddings.shape}")
 print(f"First embedding vector: {st_embeddings[0][:5]}...")  # Print the first 5 values of the first embedding vector
+print("---Similarità coseno sentence Transformers---")
+if "st_embeddings" in locals() and isinstance(st_embeddings, np.ndarray):
+    similarity_matrix_st = cosine_similarity(st_embeddings)
+    print("\nSimilarità coseno (Sentence Transformers):")
+    print(np.round(similarity_matrix_st, decimals=4))
+
+    print(f"\ninterpretazione:")
+    print(f"Frase 0: {sentences[0]}")
+    print(f"Frase 1: {sentences[1]}")
+    print(f"Frase 2: {sentences[2]}")
+
+    print(f"Similarità tra frase 0 e frase 1: {similarity_matrix_st[0][1]:.4f}")
+    print(f"Similarità tra frase 0 e frase 2: {similarity_matrix_st[0][2]:.4f}")
+    print(f"Similarità tra frase 1 e frase 2: {similarity_matrix_st[1][2]:.4f}")
+else:
+    print("Errore: st_embeddings non è definito o non è un array numpy. Non è possibile calcolare la similarità coseno.")
